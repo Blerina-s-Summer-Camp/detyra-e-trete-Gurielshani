5db4945a0c07406a5d32d0b70021cfbf1a2af229
Gurielshani <110973085+Gurielshani@users.noreply.github.com>
2022-08-22 23:04:15 +0200

    Guri Elshani

---
+++ det b.py
@@ -0,0 +1,7 @@
+Numrat = {11, 19, 4, 6, 18, 7, 3, 2, 10}
+
+for variabla in Numrat:
+    if variabla == "11, 19, 7, 3":
+        print("Keta numra jane tek: 11, 19, 7, 3")
+    else : 
+        print("Keta numra jane qift: 4, 6, 18, 2, 10" )
